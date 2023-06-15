3b7963131f59d52089c1731d41292e13ea997b13
mrAJAY1 <thisismeajayprakash@gmail.com>
2023-06-15 21:59:16 +0530

    recharts and key performance indicator ui - row 1

---
+++ client/src/components/BoxHeader.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import FlexBetween from "./FlexBetween";
+import { Typography, useTheme } from "@mui/material";
+import { Box } from "@mui/system";
+
+type Props = {
+  title: string;
+  sideText: string;
+  icon?: React.ReactNode;
+  subtitle?: string;
+};
+
+const BoxHeader = ({ icon, title, subtitle, sideText }: Props) => {
+  const { palette } = useTheme();
+  return (
+    <FlexBetween color={palette.grey[400]} margin="1.5rem 1rem 0 1rem">
+      <FlexBetween>
+        {icon}
+        <Box width="100%">
+          <Typography variant="h4" mb="-0.1rem">
+            {title}
+          </Typography>
+          <Typography variant="h6">{subtitle}</Typography>
+        </Box>
+      </FlexBetween>
+      <FlexBetween>
+        <Typography
+          variant="h5"
+          fontWeight="700"
+          color={palette.secondary[500]}>
+          {sideText}
+        </Typography>
+      </FlexBetween>
+    </FlexBetween>
+  );
+};
+
+export default BoxHeader;
--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -1,4 +1,5 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { GetKpisResponse } from "./types";
 
 export const api = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_BASE_URL }),
@@ -6,7 +7,7 @@ export const api = createApi({
   // Kpi = key performance indicators
   tagTypes: ["Kpis"],
   endpoints: build => ({
-    getKpis: build.query<void,void>({
+    getKpis: build.query<Array<GetKpisResponse>, void>({
       query: () => "kpi/kpis/",
       providesTags: ["Kpis"],
     }),
