0f5b6879c8ac08a5004baafc73fa3d5f745caa1a
mrAJAY1 <thisismeajayprakash@gmail.com>
2023-06-15 17:57:26 +0530

    redux toolkit setup and configuration

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/dist/query";
+import { api } from "@/state/api";
 import App from "@/App";
 import "@/index.css";
 
+export const store = configureStore({
+  reducer: { [api.reducerPath]: api.reducer },
+  middleware: getDefault => getDefault().concat(api.middleware),
+});
+setupListeners(store.dispatch);
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
--- client/src/scenes/dashboard/Row1.tsx
+++ client/src/scenes/dashboard/Row1.tsx
@@ -1,7 +1,9 @@
 import DashboardBox from "@/components/DashboardBox";
+import { useGetKpisQuery } from "@/state/api";
 import React from "react";
 
 const Row1 = () => {
+  const { data } = useGetKpisQuery();
   return (
     <>
       <DashboardBox gridArea="a"></DashboardBox>
