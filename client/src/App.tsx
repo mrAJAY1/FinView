c1d911ecf0cf9e7f65df5f19be477362ccc9dd2b
mrAJAY1 <thisismeajayprakash@gmail.com>
2023-06-15 15:59:42 +0530

    navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,24 @@
-import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+import { Box, CssBaseline, ThemeProvider, createTheme } from "@mui/material";
 import { useMemo } from "react";
 import { themeSettings } from "./theme";
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "@/scenes/navbar";
 function App() {
   const theme = useMemo(() => createTheme(themeSettings), []);
   return (
     <div className="app">
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        Hello there
-      </ThemeProvider>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<div>dashBoard Page</div>} />
+              <Route path="/predictions" element={<div>predictions Page</div>} />
+            </Routes>
+          </Box>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   );
 }
--- client/src/expanded-theme.ts
+++ client/src/expanded-theme.ts
@@ -1,7 +1,7 @@
 import { Palette, PaletteColor } from "@mui/material/styles/createPalette";
 
 declare module "@mui/material/styles/createPalette" {
-  interface PalletteColor {
+  interface PaletteColor {
     [key: number]: string;
   }
   interface Palette {
