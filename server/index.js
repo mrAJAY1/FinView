007b1a7c7a90ca214386547b005baa1ae54c03b9
mrAJAY1 <thisismeajayprakash@gmail.com>
2023-06-15 18:22:02 +0530

    initial server setup

---
+++ server/index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+
+// CONFIGURATIONS
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+const PORT = process.env.PORT || 6000;
+// MONGOOSE SETUP
+const { MONGO_URL } = process.env;
+mongoose
+  .connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(async () => {
+    app.listen(PORT, () => console.log(`Server Listening on port ${PORT}`));
+  })
+  .catch(error => console.log(`${error} did not connect`));
